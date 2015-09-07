9dfd2cf1991acb2b8f2795c1ea7289e88dcf06e0
htailor <hemant.tailor@live.co.uk>
2015-09-07 21:06:29 +0100

    Update bash logger

--- logger_unit_test.sh
+++ logger_unit_test.sh
@@ -3,32 +3,32 @@
 source logger.sh
 
 function logs(){
-    logging_debug "This is a debug test message"
-    logging_info "This is an info test message"
-    logging_warn "This is a warn test message"
-    logging_critical "This is a critical test message"
-    logging_error "This is an error test message"
+    log_debug "This is a debug test message"
+    log_info "This is an info test message"
+    log_warn "This is a warn test message"
+    log_critical "This is a critical test message"
+    log_error "This is an error test message"
 }
 
 # default log level
 logs
 
 # debug level test
-logging_level DEBUG
+logging_level "DEBUG"
 logs
 
 # info level test
-logging_level INFO
+logging_level "INFO"
 logs
 
 # warn level test
-logging_level WARN
+logging_level "WARN"
 logs
 
 # critical level test
-logging_level CRITICAL
+logging_level "CRITICAL"
 logs
 
 # error level test
-logging_level ERROR
+logging_level "ERROR"
 logs
